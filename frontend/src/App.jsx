1a3cbf085f0023c2141173367c8b6c0d34b0a021
Shubhamashish1310 <98949989+Shubhamashish1310@users.noreply.github.com>
2025-05-21 13:52:10 +0530

    Some new way of setup of tailwind bug fixed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,10 @@
 import AppRoutes from './router';
-
+// In src/main.jsx or src/App.jsx
+import './index.css'; // or './App.css'
 export default function App() {
   return (
     <div className="min-h-screen bg-gray-100 text-gray-800">
+      <h1 className='text-7xl font-bold text-blue-600 bg-red-900'>Shubham</h1>
       <AppRoutes />
     </div>
   );
